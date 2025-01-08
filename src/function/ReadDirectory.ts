b8667898b2447329959ed32c3d56445fd2ff28e6
xcfio <119097812+xcfio@users.noreply.github.com>
2025-01-08 20:24:14 +0600

    Add ReadDirectory function to recursively list files in a directory

---
+++ src/function/ReadDirectory.ts
@@ -0,0 +1,30 @@
+import { PathLike, readdirSync, statSync } from "node:fs"
+import { join } from "node:path"
+import { debug } from "./debug"
+
+function get_all_files(BasePath: PathLike): Array<string> {
+    try {
+        let result: Array<string> = []
+        const files = readdirSync(BasePath)
+
+        for (const file of files) {
+            const path = join(BasePath.toString(), file)
+            const stats = statSync(path)
+            stats.isDirectory() ? (result = result.concat(get_all_files(path))) : result.push(path)
+        }
+
+        return result.map((file) => file.replace(/\\/g, "/"))
+    } catch (error) {
+        debug.error(error)
+        throw error
+    }
+}
+
+export function ReadDirectory(path: PathLike): Array<string> {
+    try {
+        return get_all_files(path).map((file) => file.replace(`${path.toString()}/`, ""))
+    } catch (error) {
+        debug.error(error)
+        throw error
+    }
+}
--- src/function/index.ts
+++ src/function/index.ts
@@ -1,2 +1,3 @@
+export * from "./ReadDirectory"
 export * from "./CTSError"
 export * from "./debug"
